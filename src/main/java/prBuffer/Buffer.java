40e872a98dec23d7451d84a668972b8abf9afbb4
Jose Luis Martin <jlmmmorales@gmail.com>
2017-11-03 13:04:56 +0100

    Empezamos en casa

--- src/main/java/prBuffer/Buffer.java
+++ src/main/java/prBuffer/Buffer.java
@@ -1,4 +1,4 @@
-package buffer;
+package prBuffer;
 
 import java.util.LinkedList;
 import java.util.Queue;
--- src/test/java/prBuffer/BufferTest.java
+++ src/test/java/prBuffer/BufferTest.java
@@ -1,4 +1,4 @@
-package buffer;
+package prBuffer;
 
 import junit.framework.Test;
 import junit.framework.TestCase;
